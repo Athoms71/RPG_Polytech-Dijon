5a5277b492e2ff19e4a1f1d6e3caf494f0fc5139
augustinprotin <augugu78@gmail.com>
2024-04-11 16:59:14 +0200

    avancement chapitre 1

--- combat.py
+++ combat.py
@@ -11,14 +11,14 @@ def bataille(personnage: Ett.Joueur, ennemi: Ett.Monstre):
     """tant que le joueur et le(s) ennemi(s) on 1 PV ou plus, on alterne entre le tour du joueur et celui du monstre"""
     combat = 1
     while (combat):
-        actionDuTourJoueur(personnage, ennemi)
-        actionDuTourMonstre(personnage, ennemi)
+        action_du_tour_joueur(personnage, ennemi)
+        action_du_tour_monstre(personnage, ennemi)
         if (
                 personnage.pv == 0 or ennemi.pv == 0):
             combat = 0
 
 
-def actionDuTourMonstre(personnage: Ett.Joueur, ennemi: Ett.Monstre):
+def action_du_tour_monstre(personnage: Ett.Joueur, ennemi: Ett.Monstre):
     # on prevoit quand les montres pourront faire plusieurs actions (IA du monstre)
     choix = 1
     if (int(choix) == 1):
@@ -27,7 +27,7 @@ def actionDuTourMonstre(personnage: Ett.Joueur, ennemi: Ett.Monstre):
         pass
 
 
-def actionDuTourJoueur(personnage:  Ett.Joueur, ennemi: Ett.Monstre):
+def action_du_tour_joueur(personnage:  Ett.Joueur, ennemi: Ett.Monstre):
     """effectue une action parmis celles disponible, prend en parametre les ennemis present dans le combat et le joueur"""
     choix = input("choisissez une action parmis :\n1: attaquer\n\n")
     if (int(choix) == 1):
@@ -44,7 +44,7 @@ def attaquer(source, destination):
         "voulez vous tenter une attaque critique ?\n1 : oui\n2 : non\n\n")
     if atkspe == 1:
         # par exemple si l attaque de bas (source.pc est 10, on attaque entre 0 et 20)
-        degat = 2*(source.pc) - main.DX(2*(source.pc))
+        degat = 2*(source.pc) - DX(2*(source.pc))
     else:
         degat = source.pc
 
