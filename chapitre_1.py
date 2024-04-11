476590aabf61906d7022c7abd4fa6a78c202fbcd
augustinprotin <augugu78@gmail.com>
2024-04-11 16:46:34 +0200

    dubut du chapitre 1

---
+++ chapitre_1.py
@@ -0,0 +1,15 @@
+import entite as Ett
+import combat as Cbt
+import equipement as E
+
+
+def ch1():
+    print("Après la confrontation avec le garde, vous décidez de prendre un repos bien mérité pour reprendre des forces. Vous trouvez un endroit sûr à l'abri des regards, où vous pouvez vous reposer sans craindre d'être découvert. La nuit tombe lentement, le ciel s'embrasant de teintes rouges et dorées alors que le soleil se couche à l'horizon.")
+    print("\n")
+    print("Pendant votre sommeil, des rêves troublants hantent votre esprit. Des visions de ténèbres et de lumière se mêlent dans un tourbillon de confusion, révélant des fragments d'une histoire oubliée et des énigmes mystérieuses. Vous entendez des voix murmurer dans l'obscurité, appelant votre nom et vous incitant à découvrir la vérité cachée dans les Royaumes de l'Éclipse.")
+    print("\n")
+    print("Au matin, vous vous réveillez avec un sentiment d'urgence brûlant dans votre poitrine. Vous savez maintenant que votre destin est lié à une quête plus grande que vous, une quête pour trouver les Artefacts de l'Éclipse, des objets de pouvoir ancien qui pourraient changer le cours de la guerre entre la lumière et les ténèbres.")
+    print("\n")
+    print("Votre première tâche est de retrouver la Carte des Royaumes, un artefact légendaire qui révèle la localisation des autres Artefacts de l'Éclipse. On dit que la carte est cachée dans les ruines d'une ancienne cité, perdue depuis des siècles dans les Terres Éloignées à l'est.")
+    print("\n")
+    print("Déterminé à accomplir votre mission, vous vous levez et vous mettez en route vers les Terres Éloignées, votre cœur battant avec l'excitation de l'aventure à venir et la détermination de réaliser votre destinée.")
