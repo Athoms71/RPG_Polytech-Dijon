cbec461cefc8433fd9d4cf1329bfb1e76004d564
Athoms71 <thomas.loctin@gmail.com>
2024-04-11 15:57:58 +0200

    Commit : equipement

---
+++ equipement.py
@@ -0,0 +1,31 @@
+import entite as Ett
+
+
+class Equipement():
+    def __init__(self, nom, atk, dfc, prix, poids, cat, equip=False):
+        self.nom = nom
+        self.atk = atk
+        self.dfc = dfc
+        self.prix = prix
+        self.equip = equip
+        self.poids = poids
+        self.cat = cat
+
+
+class Consommable():
+    def __init__(self, nom, atk, dfc, heal, prix, poids, cat):
+        self.nom = nom
+        self.atk = atk
+        self.dfc = dfc
+        self.heal = heal
+        self.prix = prix
+        self.poids = poids
+        self.cat = cat
+
+    def utilisation(self, joueur: Ett.Joueur):
+        if self.cat == "soin":
+            joueur.pv += self.heal
+        elif self.cat == "attaque":
+            joueur.pc += self.atk
+        elif self.cat == "defense":
+            joueur.pd += self.dfc
