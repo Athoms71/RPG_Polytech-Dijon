35602078e51a426aea56c974147ef765e4929277
Athoms71 <thomas.loctin@gmail.com>
2024-04-12 10:00:32 +0200

    Ajout de la 2e action joueur

--- combat.py
+++ combat.py
@@ -31,28 +31,34 @@ def action_du_tour_monstre(personnage: Ett.Joueur, ennemi: Ett.Monstre):
 
 
 def action_du_tour_joueur(personnage:  Ett.Joueur, ennemi: Ett.Monstre):
-    """effectue une action parmis celles disponible, prend en parametre les ennemis present dans le combat et le joueur"""
-    choix = input("choisissez une action parmi :\n1: attaquer\n\n")
-    if (int(choix) == 1):
+    """effectue une action parmi celles disponibles, prend en parametre l ennemi et le joueur"""
+    choix = input(
+        "choisissez une action parmi :\n\t1 : attaquer\n\t2 : utiliser un objet\n")
+    if int(choix) == 1:
         attaquer(personnage, ennemi, 1)
+    elif int(choix) == 2:
+        print("Voici la liste de vos objets :\n")
+        liste_inventaire = personnage.lister_inventaire_consommable()
     else:
         print("choix indisponible, votre tour est passé :)")
 
 
 def attaquer(source, destination, type_attaquant: int):
     """prend en parametre la source de l attaque et sa destination, 
-    elle peuvent chacun etre de type joueur ou monstre, puis on enleve 
+    elle peuvent chacune etre de type joueur ou monstre, puis on enleve 
     aux pv de la destinantion autant que les dégats de la source
     Si le type attaquant est 1, le joueur attaque un monstre, sinon, 
-    c est le montres qui attaque le joueur"""
+    c est le monstre qui attaque le joueur"""
     if type_attaquant == 1:
         atkspe = int(input(
-            "voulez vous tenter une attaque critique ?\n1 : oui\n2 : non\n\n"))
+            "Voulez vous tenter une attaque critique ?\n\t1 : oui\n\t2 : non\n"))
         if atkspe == 1:
             # par exemple si l attaque de bas (source.pc est 10, on attaque entre 0 et 20)
             degat = 2*(source.pc) - DX(2*(source.pc))
         else:
             degat = source.pc
+    elif type_attaquant == 0:
+        degat = source.pc
 
     resistance = destination.pd
-    destination.pv = destination.pv - (degat-resistance)
+    destination.pv -= (degat-resistance)
