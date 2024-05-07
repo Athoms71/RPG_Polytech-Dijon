f81fbee74cc922a135eca983453e6c61503d96dc
Athoms71 <thomas.loctin@gmail.com>
2024-05-07 14:50:45 +0200

    Gestion des évènements sur les boutons de l'écran titre

--- main.py
+++ main.py
@@ -22,67 +22,124 @@ def dimensions_ecran():
     return (screen_width, screen_height)
 
 
+def start_game():
+    # A coder, au clic du bouton jouer du menu d'accueil
+    print("Start")
+    global background
+    global button_play
+    global button_settings
+    global button_quit
+    global menu_actuel
+    button_play._width, button_play._height, button_play._x, button_play._y = 0, 0, window_width+10, window_height+10
+    button_settings._width, button_settings._height, button_settings._x, button_settings._y = 0, 0, window_width+10, window_height+10
+    button_quit._width, button_quit._height, button_quit._x, button_quit._y = 0, 0, window_width+10, window_height+10
+    menu_actuel = 1
+    return menu_actuel
+
+
+def fin_fenetre():
+    global keep_screen
+    keep_screen = False
+
+
+def ecran_titre():
+    button_w, button_h = window_width//5, 80
+    button_font = pygame.font.Font("font/TheWildBreathOfZelda-15Lv.ttf", 50)
+    title_font = pygame.font.Font("font/TheWildBreathOfZelda-15Lv.ttf", 125)
+    title_text = title_font.render("Les Royaumes de l'Eclipse", True, WHITE)
+    title_rect = title_text.get_rect(
+        center=(window_width//2, window_height//4))
+    button_play = Button(
+        screen,
+        window_width//2-button_w//2,
+        2*window_height//5,
+        button_w,
+        button_h,
+
+        text="Jouer",
+        font=button_font,
+        textColour=(0, 0, 0),
+        fontSize=60,
+        margin=5,
+        inactiveColour=(255, 255, 255, 128),
+        hoverColour=(210, 210, 210),
+        pressedColour=(180, 180, 180),
+        radius=10,
+        onClick=start_game
+    )
+    button_settings = Button(
+        screen,
+        window_width//2-button_w//2,
+        2*window_height//5+button_h+50,
+        button_w,
+        button_h,
+
+        text="Parametres",
+        font=button_font,
+        textColour=(0, 0, 0),
+        fontSize=60,
+        margin=5,
+        inactiveColour=(255, 255, 255),
+        hoverColour=(210, 210, 210),
+        pressedColour=(180, 180, 180),
+        radius=10
+    )
+    button_quit = Button(
+        screen,
+        window_width//2-button_w//2,
+        2*window_height//5+2*(button_h+50),
+        button_w,
+        button_h,
+
+        text="Quitter",
+        font=button_font,
+        textColour=(0, 0, 0),
+        fontSize=60,
+        margin=5,
+        inactiveColour=(255, 255, 255, 128),
+        hoverColour=(210, 210, 210),
+        pressedColour=(180, 180, 180),
+        radius=10,
+        onClick=fin_fenetre)
+    return title_text, title_rect, button_play, button_settings, button_quit
+
+
+def menu_pause():
+    # A coder, menu pause en jeu
+    pass
+
+
 window_width, window_height = dimensions_ecran()
 screen = pygame.display.set_mode((window_width, window_height))
-bg_titre = pygame.image.load("img/chemin_fond_flou.png").convert()
-bg_titre = pygame.transform.scale(bg_titre, (window_width, window_height))
+background = pygame.image.load("img/chemin_fond_flou.png").convert()
+background = pygame.transform.scale(background, (window_width, window_height))
 
-title_font = pygame.font.Font("font/TheWildBreathOfZelda-15Lv.ttf", 90)
-title_text = title_font.render("Les Royaumes de l'Eclipse", True, WHITE)
-title_rect = title_text.get_rect(center=(window_width//2, window_height//4))
-
-button_w, button_h = window_width//5, 100
-button_font = pygame.font.Font("font/TheWildBreathOfZelda-15Lv.ttf", 60)
-button_color = pygame.Color(255, 255, 255, 128)
-
-# button_play_text = button_font.render("Jouer", True, BLACK, button_color)
-# button_play_rect = button_play_text.get_rect(
-#    center=(window_width//2, window_height//2))
-
-# button_settings_text = button_font.render(
-#    "Parametres", True, BLACK, button_color)
-# button_settings_rect = button_settings_text.get_rect(
-#   center=(window_width//2, (window_height//2)+button_h))
-
-# button_quit_text = button_font.render("Quitter", True, BLACK, button_color)
-# button_quit_rect = button_quit_text.get_rect(
-#    center=(window_width//2, (window_height//2)+2*button_h))
-
-button_play = Button(
-    screen,
-    window_width//2-button_w//2,
-    window_height//2,
-    button_w,
-    button_h,
-
-    text='Jouer',
-    font=button_font,
-    textColour=(0, 0, 0),
-    fontSize=60,
-    margin=5,
-    inactiveColour=(255, 255, 255, 128),
-    hoverColour=(255, 255, 255),
-    pressedColour=(192, 192, 192),
-    radius=10)
 
 keep_screen = True
+menu_actuel = 0
 
 while keep_screen:
     events = pygame.event.get()
     for event in events:
         dicKeys = pygame.key.get_pressed()
-        if event.type == QUIT or dicKeys[K_ESCAPE]:
+        if event.type == QUIT or dicKeys[K_TAB]:
             pygame.quit()
             keep_screen = False
             quit()
-    screen.blit(bg_titre, (0, 0))
-    screen.blit(title_text, title_rect)
+        if dicKeys[K_ESCAPE] and menu_actuel == 1:
+            menu_pause()
+    if menu_actuel == 0:
+        title_text, title_rect, button_play, button_settings, button_quit = ecran_titre()
+        screen.blit(title_text, title_rect)
+    # elif menu_actuel == 1:
+
+    screen.blit(background, (0, 0))
     pygame_widgets.update(events)
     pygame.display.flip()
 
 
 '''def scenario(chapitre: int, karma: int):
-    """lance la partie du scenario correspondant au chapitre en cours, et la voie empruniée jusqu'a présent"""
+    """lance la partie du scenario correspondant au chapitre en cours, et la voie empruniée jusqu"a présent"""
     if chapitre == 0:
         C0.intro()
         chapitre += 1
