da1d6321135b17660a1eb21d73cda28e7034ca51
Athoms71 <thomas.loctin@gmail.com>
2024-06-12 14:03:34 +0200

    Finition des fonctions de sauvegarde et de chargement de sauvegarde

--- operation_fichier.py
+++ operation_fichier.py
@@ -1,15 +1,15 @@
 import os
-import entite as Ett
 
 
 def save(dict_var: dict):
     '''
-    Sauvegarde des données de dict_var à la fin de chaque chapitre, écriture dans le fichier save.txt
-    Format du fichier save.txt :
-        nom_variable1
-        val1,val2,val3,...,valN
-        nom_variable2
-        ...
+    Sauvegarde des données de dict_var à la fin de chaque chapitre, écriture dans le fichier save.txt\n
+    Format du fichier save.txt :\n
+        nom_variable1\n
+        val1,val2,val3,...,valN\n
+        nom_variable2\n
+        ...\n
+        Toujours terminer par une ligne vide
     '''
     file = open("save.txt", "w")
     for key in dict_var:
@@ -18,8 +18,8 @@ def save(dict_var: dict):
         if type_dict_var == str:
             file.write(dict_var[key])
         elif type_dict_var == int:
-            file.write(int(dict_var[key]))
-        elif type_dict_var == list or type_dict_var == tuple:
+            file.write(str(dict_var[key]))
+        elif type_dict_var == list:
             for elt in dict_var[key]:
                 file.write(str(elt)+",")
         else:
@@ -28,15 +28,15 @@ def save(dict_var: dict):
     file.close()
 
 
-def load(save_file="save.txt"):
+def load():
     '''Chargement du fichier save_file et attribution des valeurs du fichier dans les différentes variables du jeu'''
-    if os.path.exists(save_file):
+    if os.path.exists("save.txt"):
+        file = open("save.txt", "r")
         dict_var = {}
-        file = open(save_file, "r")
         f = file.readlines()
-        for i in range(1, len(f), 2):
-            key = f[i-1][:-1]
-            values = f[i][:-1].split(',')
+        for i in range(0, len(f), 2):
+            key = f[i][:-1]
+            values = f[i+1][:-1].split(',')
             if len(values) > 1:
                 dict_var[key] = []
                 for elt in values[:-1]:
@@ -52,7 +52,35 @@ Variables à sauvegarder :
     - nom_joueur : str
     - classe_joueur : str
     - race_joueur : str
-    - inventaire_joueur : str
+    - inventaire_joueur : list
+    - pv_joueur : int
+    - argent_joueur : int
+    - main_gauche_joueur : Equipement
+    - main_droite_joueur : Equipement
+    - tete_joueur : Equipement
+    - torse_joueur : Equipement
+    - gants_joueur : Equipement
+    - jambes_joueur : Equipement
+    - pieds_joueur : Equipement
     - avancement : int
-    - 
 '''
+DICT_VAR = {            # Dictionnaire de sauvegarde
+    "nom_joueur": "Toto",
+    "classe_joueur": "guerrier",
+    "race_joueur": "humain",
+    "inventaire_joueur": ["Grosse potion", "Petite potion"],
+    "pv_joueur": 0,
+    "argent_joueur": 0,
+    "main_gauche_joueur": None,
+    "main_droite_joueur": None,
+    "tete_joueur": None,
+    "torse_joueur": None,
+    "gants_joueur": None,
+    "jambes_joueur": None,
+    "pieds_joueur": None,
+    "avancement": 0
+}
+print(DICT_VAR)
+save(DICT_VAR)
+dict_var = load()
+print(dict_var)
