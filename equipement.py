e5c3dc52b6bc5174e08e7e5efe782d9e9eb9f53e
Athoms71 <thomas.loctin@gmail.com>
2024-06-17 11:20:17 +0200

    Suppression du poids des équipements et consommables

--- equipement.py
+++ equipement.py
@@ -2,23 +2,21 @@ import entite as Ett
 
 
 class Equipement():
-    def __init__(self, nom: str, atk: int, dfc: int, prix: float, poids: float, cat: str):
+    def __init__(self, nom: str, atk: int, dfc: int, prix: int,  cat: str):
         self.nom = nom
         self.atk = atk
         self.dfc = dfc
         self.prix = prix
-        self.poids = poids
         self.cat = cat
 
 
 class Consommable():
-    def __init__(self, nom: str, atk: int, dfc: int, heal: int, prix: float, poids: float, cat: str):
+    def __init__(self, nom: str, atk: int, dfc: int, heal: int, prix: int,  cat: str):
         self.nom = nom
         self.atk = atk
         self.dfc = dfc
         self.heal = heal
         self.prix = prix
-        self.poids = poids
         self.cat = cat
 
     def utilisation(self, joueur: Ett.Joueur):
